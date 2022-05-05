9116651c1b1e3102f55c6b2c79931e5e8aa65341
Ruben Sanchez <rubensanchezcolin@gmail.com>
2022-05-05 12:23:38 -0500

    Feature image on RSS

--- functions.php
+++ functions.php
@@ -1,4 +1,19 @@
 <?php
+
+/**
+ * Featured image to RSS Feed.
+ */
+function featuredtoRSS($content) {
+    global $post;
+    if ( has_post_thumbnail( $post->ID ) ){
+    $content = '<div>' . get_the_post_thumbnail( $post->ID, 'medium', array( 'style' => 'margin-bottom: 15px;' ) ) . '</div>' . $content;
+    }
+    return $content;
+    }
+    
+    add_filter('the_excerpt_rss', 'featuredtoRSS');
+    add_filter('the_content_feed', 'featuredtoRSS');
+
 /* ----------------------------------------*
  * GENERAL *
  *----------------------------------------*/ 
