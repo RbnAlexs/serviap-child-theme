0c22695fe7eaa7d2f9756bc74d7d750d49ffb485
Ruben Sanchez <rubensanchezcolin@gmail.com>
2023-09-13 12:04:39 -0600

    Remove tidio chat from serviap hub

--- footer.php
+++ footer.php
@@ -18,7 +18,9 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 
 <?php wp_footer(); ?>
 
-<script src="//code.tidio.co/gyj5ocbc2unznxoayl8l8vozfkbmx9us.js" async></script>
+<?php if !(is_page( 'Serviap Hub' ) ):?>
+  <script src="//code.tidio.co/gyj5ocbc2unznxoayl8l8vozfkbmx9us.js" async></script>
+<?php endif; ?>
 <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left">
 		<span><i class="fa fa-angle-up"></i></span>
 </a>
